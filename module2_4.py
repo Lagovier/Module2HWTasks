fd834cdec071f35cd4103ff89ff6913086fb0546
Александр <aleksandr.lagovier@gmail.com>
2024-11-16 10:34:40 +0200

    16.11.24 10:34

--- module2_4.py
+++ module2_4.py
@@ -2,15 +2,15 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 
-for i in range(2, len(numbers)+1):
+for i in numbers:
     is_prime = True
-    for j in range(2, len(numbers)+1):
+    for j in range(2, i):
         if i / j > 1 and i % j == 0:
             is_prime = False
             break
-    if is_prime == True: #не понимаю, почему подчёркивает
+    if is_prime == True and i > 1:
         primes.append(i)
-    else:
+    elif is_prime == False and i > 1:
         not_primes.append(i)
 print("Простые числа: ",primes)
 print("Составные числа: ",not_primes)
