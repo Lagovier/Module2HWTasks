c4fcb30bf55dfa331de28242f9ec10544f0dcb63
Александр <aleksandr.lagovier@gmail.com>
2024-11-16 14:22:33 +0200

    16.11.24 14:22

--- module2_hard.py
+++ module2_hard.py
@@ -1,22 +1,24 @@
-import random
-question = range(3,21)
-q_number = random.choice(question)
-
-print("Число в первой вставке: ", q_number)
-
 def get_num_list(number):
     numbers = []
     for i in range(1, number + 1):
         numbers.append(i)
     return numbers
-num_list = get_num_list(q_number)
-answer = []
-for i in range(1, q_number + 1):
-    for j in num_list:
-        if q_number % (i + j) == 0 and i < j:
-            answer.append(i)
-            answer.append(j)
-password = str()
-for k in answer:
-    password += (str(k))
-print("Пароль:", password)
+
+while True:
+    q_number = int(input("Введите число от 3 до 20: "))
+    if q_number >= 3 and q_number <= 20:
+        num_list = get_num_list(q_number)
+        answer = []
+        for i in range(1, q_number + 1):
+            for j in num_list:
+                if q_number % (i + j) == 0 and i < j:
+                    answer.append(i)
+                    answer.append(j)
+        result = str()
+        for k in answer:
+            result += (str(k))
+        print("Пароль:", result)
+        continue
+    else:
+        print("Внимательнее! Введённое число вне заданного диапазона!")
+        continue
